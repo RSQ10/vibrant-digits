07b22487639f90ae74deeb32415f7f4cc069e911
Nishk Kumar <yorrichijr@gmail.com>
2026-04-30 19:45:11 +0530

    Refactor App component and update routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,37 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CurrencyProvider } from './context/CurrencyContext';
 
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Product from './pages/Product';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
-import { useCartSync } from "@/hooks/useCartSync";
+// Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-import Index from "./pages/Index";
-import Shop from "./pages/Shop";
-import ProductDetail from "./pages/ProductDetail";
-import Collections from "./pages/Collections";
-import CollectionDetail from "./pages/CollectionDetail";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
-import ShippingDelivery from "./pages/ShippingDelivery";
-import ReturnsExchanges from "./pages/ReturnsExchanges";
-import FAQ from "./pages/FAQ";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-
-const queryClient = new QueryClient();
-
-function AppContent() {
-  useCartSync();
-
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/product/:handle" element={<ProductDetail />} />
-      <Route path="/collections" element={<Collections />} />
-      <Route path="/collections/:handle" element={<CollectionDetail />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
+    <CurrencyProvider>
+      <Router>
 
-      {/* Support pages */}
-      <Route path="/shipping-delivery" element={<ShippingDelivery />} />
-      <Route path="/returns-exchanges" element={<ReturnsExchanges />} />
-      <Route path="/faq" element={<FAQ />} />
-      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Navbar />
 
-      {/* Fallback */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+        <Footer />
+
+      </Router>
+    </CurrencyProvider>
   );
 }
 
-export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <TooltipProvider>
-          <AppContent />
-          <Toaster />
-          <Sonner />
-        </TooltipProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
-  );
-}
+export default App;
