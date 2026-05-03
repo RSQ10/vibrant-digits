fc816fd9c8665ae2776357fb60fa44316edaa888
Nishk Kumar <yorrichijr@gmail.com>
2026-05-03 13:56:30 +0530

    Refactor routes and update page imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,37 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CurrencyProvider } from './context/CurrencyContext';
 
 // Pages
-import Home from './pages/Home';
+import Index from './pages/Index';
 import About from './pages/About';
-import Product from './pages/Product';
+import ProductDetail from './pages/ProductDetail';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
-
-// Components
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Shop from './pages/Shop';
+import Collections from './pages/Collections';
+import CollectionDetail from './pages/CollectionDetail';
+import FAQ from './pages/FAQ';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import ReturnsExchanges from './pages/ReturnsExchanges';
+import ShippingDelivery from './pages/ShippingDelivery';
 
 function App() {
   return (
     <CurrencyProvider>
       <Router>
-
-        <Navbar />
-
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
-          <Route path="/product/:id" element={<Product />} />
+          <Route path="/product/:handle" element={<ProductDetail />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/collections" element={<Collections />} />
+          <Route path="/collections/:handle" element={<CollectionDetail />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/returns-exchanges" element={<ReturnsExchanges />} />
+          <Route path="/shipping-delivery" element={<ShippingDelivery />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-
-        <Footer />
-
       </Router>
     </CurrencyProvider>
   );
